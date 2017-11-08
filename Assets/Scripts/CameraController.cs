6129c8e3c573f53974f1ebf9c22d8e200619275e
Khiemnd <khiemnd777@gmail.com>
2017-11-09 00:08:39 +0700

    adding space attribute to make a gap between field in cameracontroller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 	public Transform target;
 	public Vector3 offset;
+	[Space]
 	public float zoomSpeed = 4f;
 	public float minZoom = 5f;
 	public float maxZoom = 15f;
+	[Space]
 	public float pitch = 2f;
 	public float yawSpeed = 100f;
 
