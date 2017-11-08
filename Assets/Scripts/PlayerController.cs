91a45b173cea4d702f83af0aabd59df8af44e758
Khiemnd <khiemnd777@gmail.com>
2017-11-09 00:01:59 +0700

    commit sample of rpg environment

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask movementMask;
+
+    Camera cam;
+	PlayerMotor motor;
+
+    void Start()
+    {
+        cam = Camera.main;
+		motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100f, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+            }
+        }
+		if (Input.GetMouseButtonDown(1))
+        {
+            var ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                
+            }
+        }
+    }
+}
