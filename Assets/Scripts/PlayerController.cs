a80cae8cb4ee6cb826551b7810d8dd5b2938bc05
Khiemnd <khiemnd777@gmail.com>
2017-11-09 22:09:01 +0700

    Interactable

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour
 {
+    public Interactable focus;
+
     public LayerMask movementMask;
 
     Camera cam;
@@ -18,6 +20,11 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        // If we release left mouse
+        if(Input.GetMouseButtonUp(0)){
+            motor.Stop();
+        }
+        // If we press left mouse
         if (Input.GetMouseButton(0))
         {
             var ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -25,16 +32,38 @@ public class PlayerController : MonoBehaviour
             if (Physics.Raycast(ray, out hit, 100f, movementMask))
             {
                 motor.MoveToPoint(hit.point);
+                RemoveFocus();
             }
         }
+        // If we press right mouse
 		if (Input.GetMouseButtonDown(1))
         {
             var ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, 100f))
             {
-                
+                var interactable = hit.collider.GetComponent<Interactable>();
+                if(interactable != null){
+                    SetFocus(interactable);
+                }
             }
         }
+    }
+
+    void SetFocus(Interactable newFocus){
+        if(newFocus != focus){
+            if(focus != null)
+                focus.OnDefocused();
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+        }
+        newFocus.OnFocused(transform);
+    }
+
+    void RemoveFocus(){
+        if(focus != null)
+            focus.OnDefocused();
+        focus = null;
+        motor.StopFollowingTarget();
     }
 }
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -6,15 +6,51 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerMotor : MonoBehaviour
 {
-    NavMeshAgent agent;
+    Transform target;       // Target to follow
+    NavMeshAgent agent;     // Reference to our agent
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
     }
 
+    void Update(){
+        if(target != null){
+            agent.SetDestination(target.position);
+            FaceTarget();
+        }
+    }
+
     public void MoveToPoint(Vector3 point)
     {
+        agent.isStopped = false;
         agent.SetDestination(point);
+    }
+
+    public void FollowTarget(Interactable newTarget)
+    {
+        agent.isStopped             = false;
+        agent.stoppingDistance      = newTarget.radius * .8f;
+        agent.updateRotation        = false;   
+        target                      = newTarget.interactionTransform;
+    }
+
+    public void StopFollowingTarget()
+    {
+        agent.stoppingDistance      = 0f;
+        agent.updateRotation        = true;
+        target                      = null;
+    }
+
+    public void Stop()
+    {
+        agent.isStopped = true;
+    }
+
+    void FaceTarget()
+    {
+        var direction = (target.position - transform.position).normalized;
+        var lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 }
