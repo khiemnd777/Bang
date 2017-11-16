55250de4f33e34a231159c1069fb00b9e8ecf9a7
Khiemnd <khiemnd777@gmail.com>
2017-11-16 23:43:07 +0700

    marathon road

---
+++ Assets/Scripts/BattleFields/MarathonRunners/CharacterRunner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterRunner : MonoBehaviour
+{
+    public Image icon;
+    public Character character;
+	public float deltaSpeed = 5.625f;
+	
+	MarathonRunner marathonRunner;
+	RectTransform runnerRectTranform;
+	RectTransform rectTransform;
+	float startTime;
+    float journeyLength;
+
+	void Start(){
+		rectTransform = GetComponent<RectTransform>();
+		marathonRunner = GetComponentInParent<MarathonRunner>();
+		runnerRectTranform = marathonRunner.GetComponent<RectTransform>();
+		startTime = Time.time;
+		journeyLength = runnerRectTranform.GetWidth();
+	}
+
+    void Update()
+    {
+		var distCovered = (Time.time - startTime) * character.dexterity * deltaSpeed;
+		var fracJourney = distCovered / journeyLength;
+		rectTransform.anchoredPosition = new Vector2(Mathf.Lerp(0f, runnerRectTranform.GetWidth(), fracJourney), 0f);
+		if(fracJourney >= 1f){
+			startTime = Time.time;
+			rectTransform.anchoredPosition = Vector2.zero;
+		}
+    }
+}
---
+++ Assets/Scripts/BattleFields/MarathonRunners/MarathonRunner.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MarathonRunner : MonoBehaviour
+{
+    public Transform characterArea;
+    public Transform enemyArea;
+
+    public CharacterRunner characterRunnerPrefab;
+
+    List<CharacterRunner> characterRunners = new List<CharacterRunner>();
+    List<CharacterRunner> enemyRunners = new List<CharacterRunner>();
+
+    public void AddToCharacterArea(Character character)
+    {
+        var runner = CreateRunner(character, characterArea);
+        characterRunners.Add(runner);
+    }
+
+    public void AddToEnemyArea(Character character)
+    {
+        var runner = CreateRunner(character, enemyArea);
+        enemyRunners.Add(runner);
+    }
+
+    CharacterRunner CreateRunner(Character character, Transform parent)
+    {
+        var runner = Instantiate<CharacterRunner>(characterRunnerPrefab, parent.position, Quaternion.identity, parent);
+        runner.icon.sprite = character.icon;
+        runner.character = character;
+
+        var rt = runner.GetComponent<RectTransform>();
+        rt.anchoredPosition = new Vector2(0f, 0f);
+        rt.anchorMin = new Vector2(0f, .5f);
+        rt.anchorMax = new Vector2(0f, .5f);
+        rt = null;
+
+        return runner;
+    }
+
+    void Update(){
+        OrderCharacterRunners(characterRunners);
+        OrderCharacterRunners(enemyRunners);
+    }
+
+    void OrderCharacterRunners (List<CharacterRunner> characterRunners) {
+        var orderedCharacterRunners = characterRunners.OrderBy(x => {
+            return x.GetComponent<RectTransform>().anchoredPosition.x;
+        }).ToArray();
+
+        for(var i = 0; i < orderedCharacterRunners.Length; i++){
+            orderedCharacterRunners[i].transform.SetSiblingIndex(i);
+        }
+
+        orderedCharacterRunners = null;
+    }
+}
