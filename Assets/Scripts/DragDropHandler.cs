04d45044c2ccc761cf10dcac3deb801d14ae2c2c
Khiemnd <khiemnd777@gmail.com>
2017-11-20 22:39:22 +0700

    draggable to arrage tactic item

--- Assets/Scripts/DragDropHandler.cs
+++ Assets/Scripts/DragDropHandler.cs
@@ -7,22 +7,27 @@ using UnityEngine.EventSystems;
 [RequireComponent(typeof(Image))]
 public class DragDropHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    public bool useIcon;
     public Image icon;
+    [Space]
     public Color hoverColor;
     public bool isDrag;
     public bool isEndDrag;
-    
+
     public delegate void OnDragged(GameObject item, bool isAlternative);
     public OnDragged onDragged;
 
     DragDropHandler[] items;
     Color originalColor;
+    int orginalSiblingIndex;
     Vector3 startPosition;
     Vector3 originalIconScale;
     Vector3 lastDraggableIconPosition;
     Image draggableIcon;
+    DragDropHandler draggableObject;
+    Vector3 lastDraggableObjectPosition;
     Canvas canvas;
-    
+
     float startDragTime;
     float startEndDragTime;
     float dragJourneyLength;
@@ -38,42 +43,71 @@ public class DragDropHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     {
         canvas = GetComponentInParent<Canvas>();
         startPosition = transform.position;
+        startDragTime = Time.time;
+        dragJourneyLength = Vector3.Distance(Vector3.one, Vector3.zero);
 
-        if (icon.enabled)
+        if (useIcon)
         {
-            startDragTime = Time.time;
-            dragJourneyLength = Vector3.Distance(Vector3.one, Vector3.zero);
+            if (icon.enabled)
+            {
+                if (draggableIcon != null)
+                    Destroy(draggableIcon.gameObject);
 
-            if (draggableIcon != null)
-                Destroy(draggableIcon.gameObject);
-
-            draggableIcon = Instantiate<Image>(icon, Input.mousePosition, Quaternion.identity);
-            draggableIcon.sprite = icon.sprite;
-            draggableIcon.transform.localScale = icon.transform.lossyScale;
-            draggableIcon.transform.SetParent(canvas.transform, false);
-
-            isDrag = true;
-            isEndDrag = false;
-
-            StartCoroutine(OnBeginDragging());
+                draggableIcon = Instantiate<Image>(icon, Input.mousePosition, Quaternion.identity);
+                draggableIcon.sprite = icon.sprite;
+                draggableIcon.transform.localScale = Vector3.one; //icon.transform.lossyScale;
+                draggableIcon.transform.SetParent(canvas.transform, false);
+            }
         }
+        else
+        {
+            if (draggableObject != null)
+                Destroy(draggableObject.gameObject);
+            orginalSiblingIndex = transform.GetSiblingIndex();
+            draggableObject = Instantiate<DragDropHandler>(this, Input.mousePosition, Quaternion.identity);
+            draggableObject.transform.localScale = Vector3.one;
+            draggableObject.transform.SetParent(canvas.transform, false);
+        }
+
+        isDrag = true;
+        isEndDrag = false;
+
+        StartCoroutine(OnBeginDragging());
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (draggableIcon != null)
+        if (useIcon)
         {
-            draggableIcon.transform.position = Input.mousePosition;
-            foreach (var item in items)
+            if (draggableIcon != null)
             {
-                if (!RectTransformUtility.RectangleContainsScreenPoint(item.GetComponent<RectTransform>(), Input.mousePosition))
+                draggableIcon.transform.position = Input.mousePosition;
+                foreach (var item in items)
                 {
-                    item.transform.localScale = Vector3.one;
-                    item.GetComponent<Image>().color = originalColor;
-                    continue;
+                    if (!RectTransformUtility.RectangleContainsScreenPoint(item.GetComponent<RectTransform>(), Input.mousePosition))
+                    {
+                        item.transform.localScale = Vector3.one;
+                        item.GetComponent<Image>().color = originalColor;
+                        continue;
+                    }
+                    item.transform.localScale = Vector3.one * 1.1f;
+                    item.GetComponent<Image>().color = hoverColor;
                 }
-                item.transform.localScale = Vector3.one * 1.1f;
-                item.GetComponent<Image>().color = hoverColor;
+            }
+        }
+        else
+        {
+            if (draggableObject != null)
+            {
+                draggableObject.transform.position = Input.mousePosition;
+                for (var i = 0; i < items.Length; i++)
+                {
+                    var item = items[i];
+                    if (RectTransformUtility.RectangleContainsScreenPoint(item.GetComponent<RectTransform>(), Input.mousePosition))
+                    {
+                        transform.SetSiblingIndex(item.transform.GetSiblingIndex());
+                    }
+                }
             }
         }
     }
@@ -84,52 +118,75 @@ public class DragDropHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
         isEndDrag = true;
         dragJourneyLength = 0f;
 
-        if (draggableIcon != null)
+        startEndDragTime = Time.time;
+        endDragJourneyLength = Vector3.Distance(Vector3.one * 1.5f, Vector3.one);
+
+        if (useIcon)
         {
-            startEndDragTime = Time.time;
-            endDragJourneyLength = Vector3.Distance(Vector3.one * 1.5f, Vector3.one);
-            lastDraggableIconPosition = draggableIcon.transform.position;
-
-            DragDropHandler matchItem = null;
-            var isAlternative = false;
-            foreach (var item in items)
+            if (draggableIcon != null)
             {
-                if (RectTransformUtility.RectangleContainsScreenPoint(item.GetComponent<RectTransform>(), Input.mousePosition))
+                lastDraggableIconPosition = draggableIcon.transform.position;
+                DragDropHandler matchItem = null;
+                var isAlternative = false;
+                foreach (var item in items)
                 {
-                    if (item != this)
+                    if (RectTransformUtility.RectangleContainsScreenPoint(item.GetComponent<RectTransform>(), Input.mousePosition))
                     {
-                        if (item.icon.enabled)
+                        if (item != this)
                         {
-                            var itemIconSprite = item.icon.sprite;
-                            item.icon.sprite = icon.sprite;
+                            if (item.icon.enabled)
+                            {
+                                var itemIconSprite = item.icon.sprite;
+                                item.icon.sprite = icon.sprite;
 
-                            icon.sprite = itemIconSprite;
-                            icon.enabled = true;
-                            isAlternative = true;
+                                icon.sprite = itemIconSprite;
+                                icon.enabled = true;
+                                isAlternative = true;
+                            }
+                            else
+                            {
+                                item.icon.sprite = icon.sprite;
+                                icon.enabled = false;
+                            }
+                            item.icon.enabled = true;
+                            matchItem = item;
                         }
-                        else
-                        {
-                            item.icon.sprite = icon.sprite;
-                            icon.enabled = false;
-                        }
-                        item.icon.enabled = true;
-                        matchItem = item;
+                    }
+                    item.transform.localScale = Vector3.one;
+                    item.GetComponent<Image>().color = originalColor;
+                }
+                if (matchItem != null)
+                {
+                    Destroy(draggableIcon.gameObject);
+                    StartCoroutine(OnSlotMatch(matchItem, icon));
+                    if (matchItem.onDragged != null)
+                    {
+                        matchItem.onDragged.Invoke(this.gameObject, isAlternative);
                     }
                 }
-                item.transform.localScale = Vector3.one;
-                item.GetComponent<Image>().color = originalColor;
-            }
-            if (matchItem != null)
-            {
-                Destroy(draggableIcon.gameObject);
-                StartCoroutine(OnSlotMatch(matchItem, icon));
-                if(matchItem.onDragged != null){
-                    matchItem.onDragged.Invoke(this.gameObject, isAlternative);
+                else
+                {
+                    StartCoroutine(OnSlotMiss());
                 }
             }
-            else
-            {
-                StartCoroutine(OnSlotMiss());
+        }
+        else
+        {
+            if(draggableObject != null){
+                lastDraggableObjectPosition = draggableObject.transform.position;
+                if (orginalSiblingIndex != transform.GetSiblingIndex())
+                {
+                    Destroy(draggableObject.gameObject);
+                    StartCoroutine(OnSlotMatch(this, null));
+                    if (onDragged != null)
+                    {
+                        onDragged.Invoke(gameObject, false);
+                    }
+                }
+                else
+                {
+                    StartCoroutine(OnSlotMiss());
+                }
             }
         }
     }
@@ -141,7 +198,14 @@ public class DragDropHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
         {
             var distCovered = (Time.time - startDragTime) * 8f;
             fracJourney = distCovered / dragJourneyLength;
-            icon.transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, fracJourney);
+            if (useIcon)
+            {
+                icon.transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, fracJourney);
+            }
+            else
+            {
+                transform.localScale = Vector3.Lerp(Vector3.one / 4f, Vector3.zero, fracJourney);
+            }
             yield return null;
         }
     }
@@ -149,19 +213,35 @@ public class DragDropHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     IEnumerator OnSlotMiss()
     {
         var fracJourney = 0f;
-        while (draggableIcon != null && fracJourney < 1f)
+        while (fracJourney < 1f)
         {
             var distCovered = (Time.time - startEndDragTime) * 5f;
             fracJourney = distCovered / endDragJourneyLength;
-            icon.transform.localScale = Vector3.Lerp(Vector3.one * 1.5f, Vector3.one, fracJourney);
-
-            draggableIcon.transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, fracJourney);
-            draggableIcon.transform.position = Vector3.Lerp(lastDraggableIconPosition, startPosition, fracJourney);
+            if (useIcon)
+            {
+                if (!draggableIcon.IsNull())
+                {
+                    icon.transform.localScale = Vector3.Lerp(Vector3.one * 1.5f, Vector3.one, fracJourney);
+                    draggableIcon.transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, fracJourney);
+                    draggableIcon.transform.position = Vector3.Lerp(lastDraggableIconPosition, startPosition, fracJourney);
+                }
+            }
+            else
+            {
+                if (!draggableObject.IsNull())
+                {
+                    transform.localScale = Vector3.Lerp(Vector3.one * 1.025f, Vector3.one, fracJourney);
+                    draggableObject.transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, fracJourney);
+                    draggableObject.transform.position = Vector3.Lerp(lastDraggableObjectPosition, startPosition, fracJourney);
+                }
+            }
 
             yield return null;
         }
         if (draggableIcon != null)
             Destroy(draggableIcon.gameObject);
+        if (draggableObject != null)
+            Destroy(draggableObject.gameObject);
     }
 
     IEnumerator OnSlotMatch(DragDropHandler matchItem, Image currentIcon)
@@ -171,10 +251,16 @@ public class DragDropHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
         {
             var distCovered = (Time.time - startEndDragTime) * 5f;
             fracJourney = distCovered / endDragJourneyLength;
-            if (currentIcon.enabled)
-                currentIcon.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, fracJourney);
-
-            matchItem.icon.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, fracJourney);
+            if (useIcon)
+            {
+                if (currentIcon.enabled)
+                    currentIcon.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, fracJourney);
+                matchItem.icon.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, fracJourney);
+            }
+            else
+            {
+                transform.localScale = Vector3.Lerp(Vector3.one * 0.975f, Vector3.one, fracJourney);
+            }
 
             yield return null;
         }
