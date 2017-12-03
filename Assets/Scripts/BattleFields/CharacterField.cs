a55dd145e3f0c4a7c3ce2f0c79fe876f64f9ce41
Khiemnd <khiemnd777@gmail.com>
2017-12-03 23:30:38 +0700

    make to be coaxial

--- Assets/Scripts/BattleFields/CharacterField.cs
+++ Assets/Scripts/BattleFields/CharacterField.cs
@@ -6,46 +6,72 @@ public class CharacterField : MonoBehaviour
 {
     public bool flip;
     public Transform spawner;
-	public Character character;
+    public Character character;
 
-    void Update(){
-        Flip();
+    Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
     }
 
-	public void AddField(Character character)
+    void Update()
+    {
+        Flip();
+        BeCoaxial();
+    }
+
+    public void AddField(Character character)
     {
         this.character = character;
         SpawnModel();
         Flip();
     }
 
-	public void ClearSlot()
+    public void ClearSlot()
     {
         this.character = null;
     }
 
-    public bool CanAdd(){
+    public bool CanAdd()
+    {
         return character.IsNull();
     }
 
-    void SpawnModel(){
-        if(character.model.IsNull())
+    void SpawnModel()
+    {
+        if (character.model.IsNull())
             return;
         var model = character.model;
         model.gameObject.SetActive(true);
-        model.position = spawner.position;
+        // model.position = spawner.position;
+        model.transform.SetParent(spawner.transform);
+        model.localPosition = Vector3.zero;
+        model.localScale = Vector3.one;
         model = null;
     }
 
-    void Flip(){
-        if(!flip)
+    void Flip()
+    {
+        if (!flip)
             return;
-        if(character.IsNull())
+        if (character.IsNull())
             return;
-        if(!character.model.IsNull()){
+        if (!character.model.IsNull())
+        {
             var renderer = character.model.GetComponentInChildren<SpriteRenderer>();
             renderer.flipX = true;
             renderer = null;
         }
+    }
+
+    void BeCoaxial()
+    {
+        if (character.IsNull())
+            return;
+        if (character.model.IsNull())
+            return;
+        var eulerAngles = cam.transform.rotation.eulerAngles;
+        character.model.rotation = Quaternion.Euler(eulerAngles);
     }
 }
